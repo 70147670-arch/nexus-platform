704684cd24ece773ceb29d7119f3935e9b7acb1f
Marya <marya@gmail.com>
2026-05-16 15:34:42 +0500

    Temporary auth controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,78 +1,15 @@
-return res.json({
-
-  token: "test123",
-
-  user: {
-    id: 1,
-    email: "test@test.com",
-    role: "admin"
-  }
-
-}); 
-const User = require("../models/User");
-
-const bcrypt = require("bcryptjs");
-
-const jwt = require("jsonwebtoken");
-
-
-// REGISTER
 exports.register = async (req, res) => {
 
   try {
 
-    const {
-      name,
-      email,
-      password,
-      role
-    } = req.body;
-
-
-    // CHECK USER
-    const existingUser = await User.findOne({
-      where: { email }
-    });
-
-    if (existingUser) {
-
-      return res.status(400).json({
-        message: "User already exists"
-      });
-
-    }
-
-
-    // HASH PASSWORD
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-
-    // CREATE USER
-    const user = await User.create({
-
-      name,
-
-      email,
-
-      password: hashedPassword,
-
-      role,
-
-    });
-
-
-    res.status(201).json({
-
-      message: "User registered successfully",
-
-      user,
-
+    return res.status(200).json({
+      message: "Register Success"
     });
 
   } catch (error) {
 
-    res.status(500).json({
-      error: error.message
+    return res.status(500).json({
+      message: error.message
     });
 
   }
@@ -80,77 +17,27 @@ exports.register = async (req, res) => {
 };
 
 
-// LOGIN
+
 exports.login = async (req, res) => {
 
   try {
 
-    const {
-      email,
-      password
-    } = req.body;
+    return res.status(200).json({
 
+      token: "test123",
 
-    // FIND USER
-    const user = await User.findOne({
-      where: { email }
-    });
-
-    if (!user) {
-
-      return res.status(404).json({
-        message: "User not found"
-      });
-
-    }
-
-
-    // CHECK PASSWORD
-    const isMatch = await bcrypt.compare(
-      password,
-      user.password
-    );
-
-    if (!isMatch) {
-
-      return res.status(400).json({
-        message: "Invalid password"
-      });
-
-    }
-
-
-    // JWT TOKEN
-    const token = jwt.sign(
-
-      {
-        id: user.id,
-        role: user.role
-      },
-
-      process.env.JWT_SECRET,
-
-      {
-        expiresIn: "7d"
+      user: {
+        id: 1,
+        email: "test@test.com",
+        role: "admin"
       }
-
-    );
-
-
-    res.json({
-
-      message: "Login successful",
-
-      token,
-
-      user,
 
     });
 
   } catch (error) {
 
-    res.status(500).json({
-      error: error.message
+    return res.status(500).json({
+      message: error.message
     });
 
   }
